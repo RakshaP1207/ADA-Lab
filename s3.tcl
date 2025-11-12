b7bcd5692fde5b7221e209a1c0392f1f0b05da02
Raksha P <raksha.23cs112@sode-edu.in>
2025-11-12 22:32:56 +0530

    Update s3.tcl

--- s3.tcl
+++ s3.tcl
@@ -41,7 +41,33 @@ set tcp1 [new Agent / TCP / Vegas ]
 $ns attach - agent $n1 $tcp1
 set sink 1 [newAgent / TCPSink ]
 $ns attach - agent $n7 $sink1
-set ftp 1 [new Application / FTP] 
-$ftp1
+set ftp1 [new Application / FTP] 
+$ftp1 attach - agent $tcp 1
+$ns connect $tcp1 $sink1
+$tcp1 set class _ 1
+$tcp1 set packetsize _ 55
+set tfile1 [open cwnd1.tr w]
+$ tcp1 attach $tfile1
+$ tcp1 trace cwnd_
+
+set tcp2 [new Agent / TCP / Reno]
+$ns attach - agent $n2 $tcp2
+set sink2 [new Agent / TCPSink]
+$ns attach - agent $n8 $sink2
+set ftp2 [new Application / FTP]
+$ftp2 attach - agent $tcp2
+$ns connect $ tcp2 $sink2
+$tcp2 set class_2
+$tcp set packetSize_55
+set tfile2 [open cwnd.tr w]
+$tcp2 attach $tfile2
+$tcp2 trace cwnd_
+$ns at 0.5 "ftp1 start"
+$ns at 1.0 "ftp2 start"
+$ns at 5.0 "ftp2 stop"
+$ns at 5.0 "ftp1 stop"
+$ns at 5.5 "finish"
+$ns run
+ 
 
  
